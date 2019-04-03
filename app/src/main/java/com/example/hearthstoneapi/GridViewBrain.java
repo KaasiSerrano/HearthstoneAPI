5ea5701dd56f5bec3fb9afa063a804c3e6237676
kaasiSerrano <kaasi.serrano@gmail.com>
2019-04-02 22:53:14 -0400

    Commit 4.1

--- app/src/main/java/com/example/hearthstoneapi/GridViewBrain.java
+++ app/src/main/java/com/example/hearthstoneapi/GridViewBrain.java
@@ -22,6 +22,7 @@ public class GridViewBrain extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.favorites_page);
 
 
